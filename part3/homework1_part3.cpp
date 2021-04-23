c7ea2b8bd1877aa9de2e90ab586058c77712ccd6
Cooper <cwfaber@ucsc.edu>
2021-04-22 19:47:50 -0700

    light final commit

--- part3/homework1_part3.cpp
+++ part3/homework1_part3.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 using namespace std::chrono;
 
-
+//e f f i c i e n c y
 void sequential_comp(volatile int *a){
     for(int i = 0; i < 1024; i++){
         for(int j = 0; j < 524288; j++){
@@ -14,6 +14,7 @@ void sequential_comp(volatile int *a){
     }
 }
 
+//basically straight from the slides
 void robin_comp(volatile int *b, int tid, int thr_count){
     for(int i = tid; i < 1024; i+= thr_count){
         for(int j = 0; j < 524288; j++){
@@ -22,31 +23,29 @@ void robin_comp(volatile int *b, int tid, int thr_count){
     }
 }   
 
+//attemped to combine unrolling from part 1 with multithreading
 void fancy_comp(volatile int *c, int tid, int thr_count){
-    for(int i = tid; i < 1024; i+= thr_count){
+    for(int i = tid; i < 1024; i+= thr_count*16){
         for(int j = 0; j < 524288; j++){
             c[i]++;
+            c[i+thr_count]++;
+            c[i+thr_count*2]++;
+            c[i+thr_count*3]++;
+            c[i+thr_count*4]++;
+            c[i+thr_count*5]++;
+            c[i+thr_count*6]++;
+            c[i+thr_count*7]++;
+            c[i+thr_count*8]++;
+            c[i+thr_count*9]++;
+            c[i+thr_count*10]++;
+            c[i+thr_count*11]++;
+            c[i+thr_count*12]++;
+            c[i+thr_count*13]++;
+            c[i+thr_count*14]++;
+            c[i+thr_count*15]++;
         }
     }
 }   
-
-void thread_init(int opcode, volatile int *b, int thr_count){
-    std::thread threads[thr_count];
-    int tid;
-    for(int i = 0; i < thr_count; i++){
-        tid = i % thr_count;
-        if(opcode == 1)threads[tid] = std::thread(robin_comp, b, tid, thr_count);
-        else if(opcode == 2){
-            threads[tid] = std::thread(fancy_comp, b, tid, thr_count);
-        }
-    }
-    for(int i = 0; i < thr_count; i++){
-        threads[i].join();
-    }
-    for(int i = 0; i < thr_count; i++){
-        threads[i].~thread();
-    }
-}
 
 int main(int argc, char *argv[]){
     //check for argument thread count
@@ -62,7 +61,6 @@ int main(int argc, char *argv[]){
             thr_count = 8;
         }
     }
-
     //initialize arrays, fill with zeros
     volatile int a[1024];
     volatile int b[1024];
@@ -72,7 +70,9 @@ int main(int argc, char *argv[]){
         b[i] = 0;
         c[i] = 0;
     }
-
+    //init threads
+    std::thread threads[thr_count];
+    int tid;
     //nicked from part 1, sequential (reference) timing
     auto sequential_start = high_resolution_clock::now();
     sequential_comp(a);
@@ -80,18 +80,35 @@ int main(int argc, char *argv[]){
 
     //round robin (part 2) timing
     auto robin_start = high_resolution_clock::now();
-    thread_init(1, b, thr_count);
+    for(int i = 0; i < thr_count; i++){
+        tid = i % thr_count;
+        threads[tid] = std::thread(robin_comp, b, tid, thr_count);
+    }
+    for(int i = 0; i < thr_count; i++){
+        threads[i].join();
+    }
     auto robin_end = high_resolution_clock::now();
 
     //personal attempt (part 3) timing
     auto fancy_start = high_resolution_clock::now();
-    thread_init(2, c, thr_count);
+    for(int i = 0; i < thr_count; i++){
+        tid = i % thr_count;
+        threads[tid] = std::thread(fancy_comp, c, tid, thr_count);
+    }
+    for(int i = 0; i < thr_count; i++){
+        threads[i].join();
+    }
     auto fancy_end = high_resolution_clock::now();
+
     //correctness check
     for(int i = 0; i < 1024; i++){
         assert(a[i] == b[i]);
         assert(a[i] == c[i]);
+        assert(b[i] == c[i]);
     }
+
+    //print end outputs
+    //raw time isn't requested, but happily provided
     auto sequential_duration = duration_cast<nanoseconds>(sequential_end - sequential_start);
     double sequential_seconds = sequential_duration.count()/1000000000.0;
     cout << "sequential computation time: " << sequential_seconds << endl; 
@@ -104,7 +121,6 @@ int main(int argc, char *argv[]){
     cout << "relative round robin speedup: " << sequential_seconds/robin_seconds << endl;
     cout << "relative personal attempt speedup: " << sequential_seconds/fancy_seconds << endl;
     cout << "speedup between robin and personal: " << robin_seconds/fancy_seconds << endl; 
-
 }
 
 
